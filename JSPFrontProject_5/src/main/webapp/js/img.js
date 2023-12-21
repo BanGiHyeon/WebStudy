36c8731cfd99b6dc9de42ae698edf4064a281ef8
BANN <BANN@bbanggi>
2023-12-22 08:54:37 +0900

    1111

---
+++ JSPFrontProject_5/src/main/webapp/js/img.js
@@ -0,0 +1,11 @@
+window.onload=function(){
+let img=document.createElement('img')//<img>
+	img.src='../image/m3.jpg'
+	img.width=200
+	img.height=250
+	img.title="영화명"
+	//<img src="../image/m3.jpg" width="200" height="250" title="영화명">
+	document.body.appendChild(img)
+	//       ====
+}
+	
